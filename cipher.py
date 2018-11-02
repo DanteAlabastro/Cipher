b6316560eb85ff580c867ea416411f25d9f65693
DanteAlabastro <dante.aa@gmail.com>
2018-11-02 14:13:01 -0700

    Cipher text by key in combine_2

--- cipher.py
+++ cipher.py
@@ -1,9 +1,13 @@
+import hashlib
+from hashlib import sha256
+
 letter = 'a'
 n = 1
 # input - strip white space and punctuation. all lowercase.
 
 # key
 key = 'tests'
+hash_key = sha256(str.encode(key)).hexdigest()
 
 # key as list
 keyL = list(key)
@@ -52,11 +56,35 @@ list1 = [1, 2, 3, 4]
 
 list2 = [5, 6, 7, 8]
 
-
-def combine():
+# Add two lists
+def combine_0():
     i = 0
     while i < len(list1):
         list1[i] += list2[i]
         i += 1
     print(list1)
 
+
+def combine_1():
+    i = 0
+    while i < len(list1):
+        list1[i] += list2[i]
+        i += 1
+    print(list1)
+
+def combine_2():
+    cipher = []
+    i = 0
+    while i < len(text):
+        d = (ord(text[i]) - 96) + (ord(key[i]) - 96)
+        if d > 26:
+            # Subtract 26 and add 96
+            cipher.append(chr(d + 70))
+            i += 1
+        else:
+            cipher.append(chr(d + 96))
+            i += 1
+    print(''.join(cipher))
+
+
+# cipher[i] = chr(ord(text[i]) + ord(key[i]))
