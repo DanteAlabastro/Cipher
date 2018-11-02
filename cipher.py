9e7ff7da79d03cc7379562f89ec50e2ecca211d9
DanteAlabastro <dante.aa@gmail.com>
2018-11-02 14:36:35 -0700

    Formatting and naming

--- cipher.py
+++ cipher.py
@@ -1,30 +1,30 @@
-import hashlib
-from hashlib import sha256
+# import hashlib
+# from hashlib import sha256
 
-letter = 'a'
-n = 1
+# hash_key = sha256(str.encode(key)).hexdigest()
+
 # input - strip white space and punctuation. all lowercase.
 
 # key
 key = 'tests'
-hash_key = sha256(str.encode(key)).hexdigest()
-
-# key as list
-keyL = list(key)
-
-# key fit text len
-key_fit = 1
 
 # Text
 text = 'hello'
 
-# Encrypted  'Character + int = chr(ord(a)+n)'
-encrypted = [chr(ord(i) + ord('s') - 90) for i in list(text)]
+# Caesar cipher 'Character + int = chr(ord(a)+n)'
+def caesar(amount):
+    shifted = [chr(ord(i) + amount) for i in list(text)]
+    print(''.join(shifted))
+
 
 # Return
 print(text)
-print(''.join(encrypted))
 
+print(key)
+
+list1 = [1, 2, 3, 4]
+
+list2 = [5, 6, 7, 8]
 
 # '''
 # >>> a = ord('a') - 96
@@ -35,8 +35,7 @@ print(''.join(encrypted))
 
 
 # Add two characters
-
-def c(a, b):
+def letter_test(a, b):
     d = (ord(a) - 96) + (ord(b) - 96)
     if d > 26:
         # Subtract 26 and add 96
@@ -44,47 +43,28 @@ def c(a, b):
     else:
         print(chr(d + 96))
 
-
-def encrypt():
-    for i in text:
-        print(i)
-
-
-encrypt()
-
-list1 = [1, 2, 3, 4]
-
-list2 = [5, 6, 7, 8]
-
 # Add two lists
-def combine_0():
+def combine_lists():
     i = 0
     while i < len(list1):
         list1[i] += list2[i]
         i += 1
     print(list1)
 
-
-def combine_1():
-    i = 0
-    while i < len(list1):
-        list1[i] += list2[i]
-        i += 1
-    print(list1)
-
-def combine_2():
-    cipher = []
+# Shift text by key
+def cipher():
+    ciphertext = []
     i = 0
     while i < len(text):
         d = (ord(text[i]) - 96) + (ord(key[i]) - 96)
         if d > 26:
             # Subtract 26 and add 96
-            cipher.append(chr(d + 70))
+            ciphertext.append(chr(d + 70))
             i += 1
         else:
-            cipher.append(chr(d + 96))
+            ciphertext.append(chr(d + 96))
             i += 1
-    print(''.join(cipher))
+    print(''.join(ciphertext))
 
 
-# cipher[i] = chr(ord(text[i]) + ord(key[i]))
+cipher()
