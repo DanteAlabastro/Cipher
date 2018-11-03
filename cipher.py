48032a8a9f085528c5494c7a28d7242bddddbceb
DanteAlabastro <dante.aa@gmail.com>
2018-11-02 23:11:19 -0700

    Encryption and decryption are functioning within the console.

--- cipher.py
+++ cipher.py
@@ -1,13 +1,10 @@
-# import hashlib
-# from hashlib import sha256
-# hash_key = sha256(str.encode(key)).hexdigest()
 import re
 
 
 print('\n', '{:^80}'.format('Dante\'s de Vigenére cipher\n'))
 print('{:^80}'.format('*Numbers, spaces and special characters are removed.*'))
 
-#Menu
+# Menu
 def menu():
     toggle = True
     while toggle:
@@ -15,8 +12,7 @@ def menu():
         if choice in ('n', 'e', 'en', 'encrypt', 'encryption'):
             return user_input()
         elif choice in ('d', 'de', 'decrypt', 'decryption'):
-            print("\n Not yet!")
-            toggle = False
+            return user_input_decrypt()
         else:
             print("\n Sorry, I couldn't understand that. \n")
             return menu()
@@ -34,13 +30,34 @@ def user_input():
 
         while len(clean_key) < len(clean_text):
             print('\n', '{:^80}'.format('Please enter a cipher key longer than {} characters.'.format(len(clean_text))))
-            print('{:^80}'.format('*Numbers, spaces and special characters are removed.*'))
+            print('{:^82}'.format('*Numbers, spaces and special characters are removed.*'))
             input_key = input('\nEnter the cipher key.')
             clean_key = re.sub('[^a-z]', '', input_key.lower())
         print(clean_key)
 
         input('\nPress Enter to encrypt...')
         cipher2(clean_text, clean_key)
+        toggle = False
+
+def user_input_decrypt():
+    toggle = True
+    while toggle:
+        input_text = input('\nEnter your encrypted message...')
+        clean_text = re.sub('[^a-z]', '', input_text.lower())
+        print(clean_text)
+
+        input_key = input('\nEnter the cipher key...')
+        clean_key = re.sub('[^a-z]', '', input_key.lower())
+
+        while len(clean_key) < len(clean_text):
+            print('\n', '{:^80}'.format('Please enter a cipher key longer than {} characters.'.format(len(clean_text))))
+            print('{:^82}'.format('*Numbers, spaces and special characters are removed.*'))
+            input_key = input('\nEnter the cipher key.')
+            clean_key = re.sub('[^a-z]', '', input_key.lower())
+        print(clean_key)
+
+        input('\nPress Enter to decrypt...')
+        decrypt(clean_text, clean_key)
         toggle = False
 
 
@@ -62,6 +79,26 @@ def cipher2(text2, key2):
     joined_ciphertext = (''.join(ciphertext)).upper()
     print('\n', '{:^80}'.format('Your encrypted message is:'))
     print('\n', '{:^80}'.format(joined_ciphertext))
+
+    return menu()
+
+def decrypt(encrypted, key):
+    cleartext = []
+    i = 0
+
+    while i < len(encrypted):
+        d = (ord(encrypted[i]) - 96) - (ord(key[i]) - 96)
+        if d < 0:
+            # Add 26 and 96
+            cleartext.append(chr(d + 122))
+            i += 1
+        else:
+            cleartext.append(chr(d + 96))
+            i += 1
+
+    joined_cleartext = (''.join(cleartext)).upper()
+    print('\n', '{:^80}'.format('Your decrypted message is:'))
+    print('\n', '{:^80}'.format(joined_cleartext))
 
     return menu()
 
